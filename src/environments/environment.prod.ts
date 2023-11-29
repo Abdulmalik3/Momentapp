362904f4d6ff984ad0c283f88a14a0ac36d4b009
Abdulmalik3 <moob2008@gmail.com>
2023-11-30 01:14:15 +0300

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
   production: true
+  ,
+  supabase: {
+    url: 'https://qupalcyhiytufftknrzr.supabase.co',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF1cGFsY3loaXl0dWZmdGtucnpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM3OTkyMzIsImV4cCI6MTk5OTM3NTIzMn0.KfwrYqf7akt7qZdA2eSZ-5eAkWfusMgbUc7wi5m60FQ',
+  },
 };
